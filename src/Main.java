4e662e44a529c745b1894484e60db2670b4e4825
Leejun14 <777ljy777@gmail.com>
2025-12-09 17:10:42 +0900

    refactor: 객체 매개변수명 변경

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        Dog dog1 = new Dog("바둑이", "시골잡종");
-        Dog dog2 = new Dog("초코", "푸들");
-        Dog dog3 = new Dog("엘리자베스","포메라니안");
+        Dog dog1 = new Dog("멍멍이", "말티즈");
+        Dog dog2 = new Dog("초코", "진돗개");
+        Dog dog3 = new Dog("뭉치","푸들");
 
         System.out.println(dog1.name + " " + dog1.breed);
         System.out.println(dog2.name + " " + dog2.breed);
