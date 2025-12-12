aca69611328bb2a59df7088dac7ffa561a0b2cc5
Leejun14 <777ljy777@gmail.com>
2025-12-12 16:57:50 +0900

    feature: Dog 필드 캡슐화 진행 후 bark 메서드 생성

--- src/Dog.java
+++ src/Dog.java
@@ -1,9 +1,20 @@
 public class Dog {
-    String name;
-    String breed;
+    private final String name;
+    private final String breed;
 
     public Dog(String name, String breed){
         this.name = name;
         this.breed = breed;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getBreed(){
+        return this.breed;
+    }
+
+    public void bark(){
+        System.out.println(this.name + "가 짖고있다.");
     }
 }
